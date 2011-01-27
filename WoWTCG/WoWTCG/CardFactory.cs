9079905f051b50e9f932d2c54bae4b94ab6efcb8
DAloG <demedeckie@gmail.com>
2011-01-28 00:07:25 +0200

    Basic libriary support

--- WoWTCG/WoWTCG/CardFactory.cs
+++ WoWTCG/WoWTCG/CardFactory.cs
@@ -7,9 +7,26 @@ namespace WoWTCG
 {
 	public class CardFactory
 	{
+		private Dictionary<Hero, Deck> _libriary;
+
+		public CardFactory()
+		{
+			_libriary = ParseLib();
+		}
+
+		private Dictionary<Hero, Deck> ParseLib()
+		{
+			var lib = new Dictionary<Hero, Deck>();
+
+			return lib;
+		}
+
 		public Player NewPlayer()
 		{
-			throw new System.NotImplementedException();
+			var rand = new Random();
+
+			var pair = _libriary.ElementAt(rand.Next(_libriary.Count-1));
+			return new Player(pair.Value, pair.Key);
 		}
 	}
 }
