16b99d50f1a68630bc7ef4edfe377620b50a6853
DAloG <demedeckie@gmail.com>
2011-02-08 01:59:00 +0200

    DBDriver

---
+++ WoWTCG/DBDriver/Driver.fs
@@ -0,0 +1,26 @@
+﻿// Learn more about F# at http://fsharp.net
+namespace WoWTCGDBDriver
+
+open System.Runtime.Serialization
+
+
+open System.IO
+
+module Accessor =     
+    let desave<'T> (fileName:string) =
+        let xmlSerializer = DataContractSerializer(typeof<'T>); 
+        use fs = File.OpenRead(fileName)
+        xmlSerializer.ReadObject(fs) :?> 'T
+
+    let loadFromString (xml:string) = 
+        let xmlSerializer = DataContractSerializer(typeof<'T>);  
+        let reader = System.Xml.XmlReader.Create(new StringReader(xml))
+        xmlSerializer.ReadObject(reader) :?> 'T
+
+    let Cards (fileName:string) = 
+        let cards:Program.FullCard[] = desave fileName
+        cards
+
+    let CardsFromString (xml:string) = 
+        let cards:Program.FullCard[] = loadFromString xml
+        cards
---
+++ WoWTCG/DBDriver/Script.fsx
@@ -0,0 +1,9 @@
+﻿// This file is a script that can be executed with the F# Interactive.  
+// It can be used to explore and test the library project.
+// Note that script files will not be part of the project build.
+#r "System.Runtime.Serialization"
+
+#load "Driver.fs"
+open WoWTCGDBDriver
+
+Accessor.Cards "e:\Coding\WoW TCG\WoWTCG\WoWTCG\Resources\cards_full.xml"
